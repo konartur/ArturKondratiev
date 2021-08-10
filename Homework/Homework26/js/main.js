9c298b7b08e58a71f88c33e1d86fd9f8d57f9853
konartur <suntikflood@gmail.com>
2021-08-10 23:25:55 +0300

    burger is worked

--- Homework/Homework26/js/main.js
+++ Homework/Homework26/js/main.js
@@ -3,14 +3,34 @@ let burger = document.querySelector('.header__hamburger')
 let burgerOpen = false;
 let menu = document.querySelector('.nav');
 
+let onresize = function () {
+    let width = document.body.clientWidth;
+
+    if(width > 950 ){
+        menu.classList.remove('d-none')
+        menu.classList.add('d-flex')  
+    }
+    else if (width <= 950 && !burger.classList.contains('open')) {
+        menu.classList.remove('d-flex')
+        menu.classList.add('d-none')
+    } else if (width <= 950 && burger.classList.contains('open')) {
+        menu.classList.remove('d-none')
+        menu.classList.add('d-flex')
+    }
+};
+window.addEventListener('resize', onresize);
+
+
 burger.addEventListener('click', () => {
-    if(!burgerOpen) {
-        burger.classList.add('open')
-        menu.style.display = 'flex';
-        burgerOpen = true
-    } else {
+    if(burgerOpen) {
         burger.classList.remove('open')
-        menu.style.display = 'none';
+        menu.classList.add('d-none')
+        menu.classList.remove('d-flex')
         burgerOpen = false
+    } else {
+        burger.classList.add('open')
+        menu.classList.add('d-flex')
+        menu.classList.remove('d-none')
+        burgerOpen = true
     }
 })
