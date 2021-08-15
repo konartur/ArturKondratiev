4cad9fef97bcdc784de9729b2cc6fe2fa1bbe26f
konartur <suntikflood@gmail.com>
2021-08-15 14:23:52 +0300

    created footer

--- Homework/Homework26/js/main.js
+++ Homework/Homework26/js/main.js
@@ -2,11 +2,62 @@
 let burger = document.querySelector('.header__hamburger')
 let burgerOpen = false;
 let menu = document.querySelector('.nav');
-let maxWidth = 991;
+let maxWidth =  991;
+
+// Swiper
+const swiper = new Swiper('.swiper-container', {
+  direction: 'horizontal',
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  // scrollbar: {
+  //   el: '.swiper-scrollbar',
+  // },
+  // speed: 400,
+  // spaceBetween: 100,
+    // Default parameters
+// slidesPerView: 7,
+// spaceBetween: 10,
+// Responsive breakpoints
+breakpoints: {
+  // when window width is >= 320px
+  320: {
+    slidesPerView: 2,
+    spaceBetween: 20
+  },
+  // when window width is >= 480px
+  480: {
+    slidesPerView: 3,
+    spaceBetween: 20
+  },
+  // when window width is >= 640px
+  640: {
+    slidesPerView: 4,
+    spaceBetween: 30
+  },
+   // when window width is >= 992px
+  992: {
+    slidesPerView: 7,
+    spaceBetween: 20
+    
+  }
+}
+});
 
 let onresize = function () {
-    let width = document.body.clientWidth;
-
+  let width = document.body.clientWidth;
+  swiperToggleByWidth(width);
+  
     if(width > maxWidth ){
         menu.classList.remove('d-none')
         menu.classList.add('d-flex')  
@@ -36,54 +87,14 @@ burger.addEventListener('click', () => {
     }
 })
 
-// Swiper
-const swiper = new Swiper('.swiper-container', {
-    direction: 'horizontal',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    // scrollbar: {
-    //   el: '.swiper-scrollbar',
-    // },
-    // speed: 400,
-    // spaceBetween: 100,
-      // Default parameters
-  // slidesPerView: 7,
-  // spaceBetween: 10,
-  // Responsive breakpoints
-  breakpoints: {
-    // when window width is >= 320px
-    320: {
-      slidesPerView: 2,
-      spaceBetween: 20
-    },
-    // when window width is >= 480px
-    480: {
-      slidesPerView: 3,
-      spaceBetween: 10
-    },
-    // when window width is >= 640px
-    640: {
-      slidesPerView: 4,
-      spaceBetween: 40
-    },
-     // when window width is >= 992px
-    992: {
-      slidesPerView: 7,
-      spaceBetween: 20
-      
-    }
+let swiperToggleByWidth = (width, laptopBreakpointWidth = 992) => {
+  if (width >= laptopBreakpointWidth) {
+    swiper.disable();
+  } else {
+    swiper.enable()
   }
-  });
+}
+swiperToggleByWidth(document.body.clientWidth);
+
+
 
