9648427e6fd515a58bc2914fa3a48280def3f65a
konartur <suntikflood@gmail.com>
2021-08-21 21:53:20 +0300

    created form

---
+++ Homework/Homework31/Form validation/script.js
@@ -0,0 +1,6 @@
+const form = document.querySelector('.form');
+let password = form.querySelector('.password').value;
+let passwordRepeat = form.querySelector('.passwordRepeat').value;
+let position = form.querySelector('.passwordPosition').value;
+let message = form.querySelector('.textarea').value;
+let btnForm = form.querySelector('.validate');
