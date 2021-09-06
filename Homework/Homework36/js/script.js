702cdc5f121e7e82e7c877ee0cccabf645559712
konartur <suntikflood@gmail.com>
2021-09-06 18:49:54 +0300

    HW 36 completed

--- Homework/Homework36/js/script.js
+++ Homework/Homework36/js/script.js
@@ -1,15 +1,26 @@
 $(document).ready(function () {
-    $('.header__btn').click(function(event) {
-       event.preventDefault();
-       $('.modal').show();
-       $('.modal__span').click(function() {
-        $('.modal').hide();
 
-        if(event.target != '.modal') {
-            $('.modal').hide();
-        }
-       });
-     
-            console.log('work   ')
-		});
+    const modal = $('.modal');
+    const overlay = modal.find('.modal__overlay');
+    const toggleBtnComment = $('.modal__comment-btn');
+    
+    $('.header__btn').click(function(event) {
+    //    event.preventDefault();
+        modal.show();
+
+    });
+    
+    $('.modal__span').click(function() {
+     modal.hide();
+    });
+
+    overlay.click(function() {
+        modal.hide();
+    });
+
+    toggleBtnComment.click(function() {
+        $('.form__message').toggle();
+    })
+
+   
 });
